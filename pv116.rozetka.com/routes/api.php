5ab9de57147049eb54b693689a2b5e09f0ac7e2f
zhensshen <101862402+zhensshen@users.noreply.github.com>
2024-01-26 17:56:55 +0200

    are u ok?

---
+++ pv116.rozetka.com/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/categories',[CategoryController::class, 'getList']);
+Route::post('/categories',[CategoryController::class, 'create']);
+Route::delete("/categories/{id}", [CategoryController::class, "delete"]);
+Route::post("/categories/edit/{id}", [CategoryController::class, "edit"]);
+
+Route::get('/products',[ProductController::class, 'getList']);
+Route::post('/product',[ProductController::class, 'store']);
+
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
+
